04f79ebffcad896e2a582d427996c719ce5199c3
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2021-09-26 21:00:23 -0500

    part 2 commit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,18 @@
 $ (document).ready(onReady);
+
 function onReady(){
-    console.log('JQ')
+    getInventory();
+}
+
+function getInventory (){
+    console.log(' in getInventory')
+    $.ajax({
+        method: 'GET',
+        url:'/inventory'
+    }).then( function(response){
+        console.log('back from GET:', response);
+    }).catch(function(err){
+        alert('server is down');
+        console.log(err);
+    })
 }
